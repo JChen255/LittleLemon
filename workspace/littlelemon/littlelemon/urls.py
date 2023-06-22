666e83a18b0dc8030c18097a669a6fa1967598d5
JChen255 <cjialin255@gmail.com>
2023-06-22 14:28:36 -0400

    Test APIs

--- workspace/littlelemon/littlelemon/urls.py
+++ workspace/littlelemon/littlelemon/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('restaurant/booking/', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('api/', include('restaurant.urls')),
 ]
