f47a9bd9b2265a266e2e7c3c3a5bf6200bc2a067
JChen255 <cjialin255@gmail.com>
2023-06-22 12:26:28 -0400

    User Authentication

--- workspace/littlelemon/littlelemon/settings.py
+++ workspace/littlelemon/littlelemon/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Set REST_FRAMEWORK
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
+# Set the DJOSER variable
+DJOSER = {"USER_ID_FIELD:":"username"}
--- workspace/littlelemon/restaurant/urls.py
+++ workspace/littlelemon/restaurant/urls.py
@@ -1,9 +1,11 @@
 #define URL route for index() view
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemView.as_view(), name='menu'),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='single_menu'),
+    path('api-token-auth/', obtain_auth_token)
 ]
